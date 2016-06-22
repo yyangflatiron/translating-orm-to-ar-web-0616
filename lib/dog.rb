d753fdb5b7266b36c09dc56bdfc03e485846ae94
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-22 14:24:41 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
-class Dog
+
+class Dog <ActiveRecord::Base
+
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS dogs(
+#     id INTEGER PRIMARY KEY,
+#     name TEXT,
+#     breed TEXT
+#     )
+# SQL
+
+# ActiveRecord::Base.connection.execute(sql)
+
+
+
+
 end
---
+++ notes.rb
@@ -0,0 +1,26 @@
+PRIVATE
+
+what belongs to migration
+altering the structure of database
+as opposed to the data itself
+
+rake - T 
+will load all the available functions in rake
+
+some gems might conflict with others
+listing all of the gems in the gemfile
+will only download the versions of the gems
+that work well with each other
+
+bundle init
+will make the gem file in the correct spot
+
+in environment.rb
+require 'bundler'
+Bundler.require
+
+
+what code is in charge of changing the structure of the database
+create table & drop table
+
+
